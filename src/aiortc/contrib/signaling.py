087f087e3f81d124257f50e3a218735ec46b27b6
Alex Zhukov <zhukov.alex@gmail.com>
2022-06-06 20:39:02 -0700

    websocket signaling

--- src/aiortc/contrib/signaling.py
+++ src/aiortc/contrib/signaling.py
@@ -1,8 +1,13 @@
 import asyncio
+import datetime
 import json
 import logging
 import os
+import ssl
 import sys
+import threading
+
+import websockets
 
 from aiortc import RTCIceCandidate, RTCSessionDescription
 from aiortc.sdp import candidate_from_sdp, candidate_to_sdp
@@ -39,6 +44,80 @@ def object_to_string(obj):
         message = {"type": "bye"}
     return json.dumps(message, sort_keys=True)
 
+
+def debug(*args) -> None:
+    print(datetime.datetime.now().time().isoformat(), threading.current_thread().name, *args, file=sys.stderr)
+
+
+class SimpleWebsocketSignaling:
+    server = "wss://zhuker.video:8443"
+    our_id = "4241"
+    remote_id = "4240"
+
+    def __init__(self, server, our_id, remote_id, request_offer: bool):
+        self.server = server
+        self.request_offer = request_offer
+        self.our_id = our_id
+        self.remote_id = remote_id
+        self.wsloop = None
+        self.wsconn = None
+
+    async def connect(self):
+        await self.websocket_connect()
+
+    async def close(self):
+        debug("close")
+        if self.wsconn is not None:
+            await self.wsconn.close()
+            self.wsconn = None
+
+    async def receive(self):
+        recv = await self.wsconn.recv()
+        debug("<", recv)
+        return object_from_string(recv)
+
+    async def send(self, descr):
+        await self.send_ws_msg(object_to_string(descr))
+
+    async def websocket_connect(self):
+        if self.server.startswith("wss://"):
+            sslctx = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+            self.wsconn = await websockets.connect(self.server, ssl=sslctx, ping_interval=5)
+        else:
+            self.wsconn = await websockets.connect(self.server, ping_interval=5)
+        await self.send_ws_msg(f'HELLO {self.our_id}')
+        msg = await self.wsconn.recv()
+        if "HELLO" != msg:
+            raise Exception("expected HELLO. got unhandled response " + msg)
+
+        if self.request_offer:
+            await self.send_ws_msg(f'SESSION {self.remote_id}')
+            msg = await self.wsconn.recv()
+            if 'SESSION_OK' == msg:
+                await self.send_ws_msg(f'OFFER_REQUEST')
+            else:
+                raise Exception("expected SESSION_OK. got unhandled response " + msg)
+        else:
+            msg = await self.wsconn.recv()
+            if 'OFFER_REQUEST' == msg:
+                debug("offer requested")
+            else:
+                raise Exception("unhandled response " + msg)
+
+    async def send_ws_msg(self, txt: str):
+        debug(f'> {txt}')
+        await self.wsconn.send(txt)
+
+    async def websocket_recvloop(self):
+        assert self.wsconn
+        async for message in self.wsconn:
+            debug(f'< {message}')
+            if 'HELLO' == message:
+                debug('registered with server as peer', self.our_id)
+                await self.send_ws_msg(f'SESSION {self.remote_id}')
+            elif 'SESSION_OK' == message:
+                debug('waiting for offer')
+                await self.send_ws_msg(f'OFFER_REQUEST')
 
 class CopyAndPasteSignaling:
     def __init__(self):
@@ -190,7 +269,16 @@ def add_signaling_arguments(parser):
     parser.add_argument(
         "--signaling",
         "-s",
-        choices=["copy-and-paste", "tcp-socket", "unix-socket"],
+        choices=["copy-and-paste", "tcp-socket", "unix-socket", "websocket"],
+    )
+    parser.add_argument(
+        "--signaling-url", default="", help="Signaling url (eg wss://server:443)"
+    )
+    parser.add_argument(
+        "--our-id", default="", help="Signaling id of this host (websocket only)"
+    )
+    parser.add_argument(
+        "--remote-id", default="", help="Signaling id of remote host to connect to (websocket only)"
     )
     parser.add_argument(
         "--signaling-host", default="127.0.0.1", help="Signaling host (tcp-socket only)"
@@ -213,5 +301,7 @@ def create_signaling(args):
         return TcpSocketSignaling(args.signaling_host, args.signaling_port)
     elif args.signaling == "unix-socket":
         return UnixSocketSignaling(args.signaling_path)
+    elif args.signaling == "websocket":
+        return SimpleWebsocketSignaling(args.signaling_url, args.our_id, args.remote_id, args.role == "answer")
     else:
         return CopyAndPasteSignaling()
